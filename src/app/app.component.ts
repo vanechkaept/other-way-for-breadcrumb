d0fdce904f3f59f611fc4cc409d59936adbc8054
vanechkaept <44510629+vanechkaept@users.noreply.github.com>
2023-02-28 22:12:58 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
+  homeCrumb = true;
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
